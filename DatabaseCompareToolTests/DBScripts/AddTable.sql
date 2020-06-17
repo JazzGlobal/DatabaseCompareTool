5a9614227556eb5d68fb9074025461598178fb17
JazzGlobal <waymer147@gmail.com>
2020-06-17 11:24:39 -0400

    Switched order that the tables are created.

--- DatabaseCompareToolTests/DBScripts/AddTable.sql
+++ DatabaseCompareToolTests/DBScripts/AddTable.sql
@@ -1,10 +1,11 @@
-﻿CREATE TABLE CustomerDatabase.dbo.Customer(
-	CustomerId int NOT NULL UNIQUE, 
-	Name varchar(255), 
-	Address varchar(255));
-
-CREATE TABLE CustomerDatabase.dbo.Project(
+﻿CREATE TABLE CustomerDatabase.dbo.Project(
 	ProjectId int NOT NULL UNIQUE, 
 	CustomerId int,
 	Name varchar(255),
 	Description varchar(255));
+
+CREATE TABLE CustomerDatabase.dbo.Customer(
+	CustomerId int NOT NULL UNIQUE, 
+	Name varchar(255), 
+	Address varchar(255));
+
