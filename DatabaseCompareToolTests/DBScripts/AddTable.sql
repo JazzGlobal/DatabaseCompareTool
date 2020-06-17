84ee37375e02918f2a867945e99424764436f7e8
JazzGlobal <waymer147@gmail.com>
2020-06-17 08:40:12 -0400

    Script now creates two relevant tables.

--- DatabaseCompareToolTests/DBScripts/AddTable.sql
+++ DatabaseCompareToolTests/DBScripts/AddTable.sql
@@ -1,2 +1,10 @@
-﻿CREATE TABLE CustomerDatabase.dbo.Customer(Name varchar(20), Address varchar(150))
+﻿CREATE TABLE CustomerDatabase.dbo.Customer(
+	CustomerId int NOT NULL UNIQUE, 
+	Name varchar(255), 
+	Address varchar(255));
 
+CREATE TABLE CustomerDatabase.dbo.Project(
+	ProjectId int NOT NULL UNIQUE, 
+	CustomerId int,
+	Name varchar(255),
+	Description varchar(255));
