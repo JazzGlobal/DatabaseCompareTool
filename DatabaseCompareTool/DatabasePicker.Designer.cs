d872352f487063364a8ce5c35780d4a2ed079b9a
JazzGlobal <waymer147@gmail.com>
2020-06-17 15:31:51 -0400

    Built form for selecting a database.

---
+++ DatabaseCompareTool/DatabasePicker.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DatabaseCompareTool
+{
+    partial class DatabasePicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.databaseList = new System.Windows.Forms.ListBox();
+            this.chooseDataBase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // databaseList
+            // 
+            this.databaseList.FormattingEnabled = true;
+            this.databaseList.ItemHeight = 16;
+            this.databaseList.Location = new System.Drawing.Point(13, 13);
+            this.databaseList.Name = "databaseList";
+            this.databaseList.Size = new System.Drawing.Size(263, 228);
+            this.databaseList.TabIndex = 0;
+            // 
+            // chooseDataBase
+            // 
+            this.chooseDataBase.Location = new System.Drawing.Point(13, 259);
+            this.chooseDataBase.Name = "chooseDataBase";
+            this.chooseDataBase.Size = new System.Drawing.Size(263, 46);
+            this.chooseDataBase.TabIndex = 1;
+            this.chooseDataBase.Text = "Select";
+            this.chooseDataBase.UseVisualStyleBackColor = true;
+            // 
+            // DatabasePicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 323);
+            this.Controls.Add(this.chooseDataBase);
+            this.Controls.Add(this.databaseList);
+            this.Name = "DatabasePicker";
+            this.Text = "Choose A Database";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox databaseList;
+        private System.Windows.Forms.Button chooseDataBase;
+    }
+}
